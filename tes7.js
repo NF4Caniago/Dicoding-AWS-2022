1594cdf841a6977e55c899aebe59c4309e5f3f8e
Afif <afifilhamcaniago@gmail.com>
2022-10-14 21:49:09 +0700

    function

---
+++ tes7.js
@@ -0,0 +1,67 @@
+const greeting = function(name, language){
+    if(language === "English") {
+        return "Good Morning " + name + "!";
+    } else if (language === "French") {
+        return "Bonjour " + name + "!";
+    } else {
+        return "Selamat Pagi " + name + "!";
+    }
+}
+
+console.log(greeting('Ron', 'English'));
+
+const user = {
+    id: 24,
+    displayName: 'kren',
+    fullName: 'Kylo Ren',
+};
+
+function introduce({displayName, fullName}){
+    console.log(`${displayName} is ${fullName}`);
+}
+
+introduce(user);
+
+function exponentsFormula(baseNumber, exponent = 2) {
+    let result = baseNumber ** exponent;
+    console.log(`${baseNumber}^${exponent} = ${result}`);
+}
+
+exponentsFormula(2);
+
+function sum(...numbers) {
+    let result = 0;
+    for (let number of numbers) {
+        result += number;
+    }
+    return result;
+}
+
+console.log(sum(1, 2, 3, 4, 5));
+
+const sayHello = name => console.log(`hello ${name}!`);
+
+sayHello("Leia");
+
+function multiply(num) {
+    total = num * num;
+    return total;
+}
+
+let total = 9;
+let number  = multiply(20);
+
+console.log(total)
+
+function init() {
+    var name = 'Obi Wan';
+
+    function greet() {
+        console.log(`Halo, ${name}`);
+    }
+
+    return greet;
+}
+
+let myFunction = init();
+myFunction();
