ba542523985719f604563c20037d4092dbf7ada8
Afif <afifilhamcaniago@gmail.com>
2022-10-15 17:55:32 +0700

    module

--- CoffeeMachine/index.js
+++ CoffeeMachine/index.js
@@ -1,7 +1,14 @@
-console.log("Menyalakan mesin kopi");
-console.log("Menggiling biji kopi");
-console.log("Memanaskan air");
-console.log("Mencampurkan air dan kopi");
-console.log("Menuangkan kopi ke dalam gelas");
-console.log("Menuangkan susu ke dalam gelas");
-console.log("Kopi Anda sudah siap!");
+//const {coffeeStock, isCoffeeMachineReady}  = require('./state');
+import {coffeeStock as stock, isCoffeeMachineReady} from "./state.js";
+
+const makeCoffee = (type, miligrams) => {
+   if (!isCoffeeMachineReady){
+       return "Mesin belum siap"
+    } else if (stock[type] >= miligrams) {
+        return "Kopi Berhasil Dibuat!";
+    } else {
+        return "Biji Kopi Habis!";
+    }
+};
+
+console.log(makeCoffee("robusta", 80));
