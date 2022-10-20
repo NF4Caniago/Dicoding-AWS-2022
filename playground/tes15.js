4cc712378e5fd678e23df20c4d390f1b4e8e7ce1
Afif <afifilhamcaniago@gmail.com>
2022-10-20 09:47:05 +0700

    done modul javascript

--- playground/tes15.js
+++ playground/tes15.js
@@ -1,34 +1,49 @@
-class NetworkError extends Error {
-    constructor(message) {
-      super(message);
-      this.name = 'NetworkError';
-    }
-  }
+// class NetworkError extends Error {
+//     constructor(message) {
+//       super(message);
+//       this.name = 'NetworkError';
+//     }
+//   }
   
-  // TODO: 1
-  const fetchingUserFromInternet = (isOffline) => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-        if (isOffline) {
-          reject(new NetworkError('Gagal mendapatkan data dari internet'));
-        }
-        resolve({ name: 'John', age: 18 });
-      }, 500);
-    });
-  };
+//   // TODO: 1
+//   const fetchingUserFromInternet = (isOffline) => {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//         if (isOffline) {
+//           reject(new NetworkError('Gagal mendapatkan data dari internet'));
+//         }
+//         resolve({ name: 'John', age: 18 });
+//       }, 500);
+//     });
+//   };
   
   
-  // TODO: 2
-  async function gettingUserName(){
-    isOffline = true;
-    try{
-        const user = await fetchingUserFromInternet(isOffline);
-        console.log(user.name);
-        return user.name;
-    } catch (rejectedReason) {
-        console.log(rejectedReason);
-        return rejectedReason;
-    }
-  };
+//   // TODO: 2
+//   async function gettingUserName(){
+//     isOffline = true;
+//     try{
+//         const user = await fetchingUserFromInternet(isOffline);
+//         console.log(user.name);
+//         return user.name;
+//     } catch (rejectedReason) {
+//         console.log(rejectedReason);
+//         return rejectedReason;
+//     }
+//   };
   
-  gettingUserName();
+//   gettingUserName();
+
+let myString = "";
+
+try {
+    myString += "a";
+    throw Error();
+} catch(e) {
+    myString += "b";
+} finally {
+    myString += "c";
+    throw Error();
+    myString += "d";
+}
+
+console.log(myString);
